b8da53d682e7f5318bc0148304ebad5eb78dadd9
Chidera Ozigbo <chideraozigbo@gmail.com>
2025-08-09 17:33:50 +0100

    fix: changed the retries time

--- dags/ngx_stock_market_dag.py
+++ dags/ngx_stock_market_dag.py
@@ -27,7 +27,7 @@ default_args = {
     "email_on_failure": False,
     "email_on_retry": False,
     "retries": 1,
-    "retry_delay": timedelta(seconds=10),
+    "retry_delay": timedelta(minutes=5),
     "on_failure_callback": task_state_alert
 }
 
